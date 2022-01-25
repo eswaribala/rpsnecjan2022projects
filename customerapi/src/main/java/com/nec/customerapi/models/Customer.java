ba170e870109fee41ecb8cc36b5a990516cba6ca
eswaribala <parameswaribala@gmail.com>
2022-01-25 10:34:13 +0530

    updated

---
+++ customerapi/src/main/java/com/nec/customerapi/models/Customer.java
@@ -0,0 +1,44 @@
+package com.nec.customerapi.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Customer_Id")
+    private long customerId;
+	@Embedded
+	private FullName name;
+	@DateTimeFormat(iso = ISO.DATE)
+    @Column(name="DOB")
+	private LocalDate dob;
+	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+	 @JsonProperty("addressList")			  
+	@JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY) 
+	private List<Address> addressList;
+	
+}
---
+++ customerapi/src/main/java/com/nec/customerapi/models/FullName.java
@@ -0,0 +1,18 @@
+package com.nec.customerapi.models;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.Data;
+
+@Data
+@Embeddable
+public class FullName {
+    @Column(name="First_Name",nullable = false,length = 50)
+	private String firstName;
+    @Column(name="Last_Name",nullable = false,length = 50)
+	private String lastName;
+    @Column(name="Middle_Name",nullable = true,length = 50)
+	private String middleName;
+	
+}
